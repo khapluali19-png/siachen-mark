7f847ce80ba430f3fa1c4c05855c42137823c82b
Basharat Ali <basharataliofficial76@gmail.com>
2026-07-26 16:00:27 +0500

    Fix next config options for Next 16

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,1 @@
-const nextConfig = { typescript: { ignoreBuildErrors: true }, eslint: { ignoreDuringBuilds: true } }; export default nextConfig;
+const nextConfig = { typescript: { ignoreBuildErrors: true } }; export default nextConfig;
