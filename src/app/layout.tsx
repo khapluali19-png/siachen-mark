80118c162d8f871eb62098295259125b35ea3e6a
Basharat Ali <basharataliofficial76@gmail.com>
2026-07-27 13:58:25 +0500

    build issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const montserrat = Montserrat({
   subsets: ["latin"],
   variable: "--font-montserrat",
 });
-
+//tst
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL ?? "https://siachenmark.com"),
   title: {
